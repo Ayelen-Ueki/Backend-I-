2bfa129efb5abc95f82aa898d0f2b7a55a3f44a0
Maria Ueki <aye_ueki_011@hotmail.com>
2024-12-15 21:06:58 +0900

    Clase 1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("Hola, mundo!")
+
+let date = new Date()
+
+console.log(date.toLocaleString())
+
+let fullname = "Maria Ayelen Ueki"
+//Expresiones regulares
+
+let regexp = /\d/
+
+console.log(regexp.test(fullname))
