e8bac4c4fc616c799e5eb9249e883f76ac63eb84
Maria Ueki <aye_ueki_011@hotmail.com>
2025-02-03 18:15:56 -0300

    Clase10

--- Clase10/public/js/index.js
+++ Clase10/public/js/index.js
@@ -3,5 +3,36 @@
 //Iniciamos la conexion desde el lado del cliente
 const socket = io(); //No tenemos que importar socket.io en este caso porque estamos manejando todo desde index.handlebars
 
-//Emitir evento
-socket.emit();
+//Emitir evento. Tenemos que asignarle un nombre y la informacion a emitir
+//socket.emit("chat message", "Hola! Saludos desde el cliente web.");
+
+//Escuchamos los eventos del servidor - Recibimos el historial de mensajes (solo se hace 1 vez al inicio)
+socket.on("welcome", (messages)=>{
+    //Capturamos el chatbox, que es donde se muestran los mensajes en el html
+    const chatbox = document.getElementById("chatbox");
+    messages.forEach(({id,message}) => { //Desestructuracion de las propiedades del array de messages
+        //Mostramos los mensajes en el html
+        chatbox.innerHTML += `<p> ${id}: ${message} </p>`
+    });
+})
+
+//Manejo de formulario para el chat
+//necesitamos capturar informacion hrml desde JavaScript
+const formChat = document.getElementById("formChat");
+const inputMessage =  document.getElementById("inputChat");
+
+//Escuchamos eventos del navegador (en este caso cuando se envie el formulario)
+formChat.addEventListener("submit", (event)=>{
+    event.preventDefault(); //Para evitar que al enviar el formulario se recarge la pagina por defecto
+    const newMessage = inputMessage.value;
+    inputMessage.value = "";//Para que se resetee
+
+    socket.emit("chatMessage",newMessage);
+})
+
+//Recibimos el mensaje enviado por uno de los clientes a todos los clientes
+socket.on("broadcastMessage", ({id, message})=>{
+    const chatbox = document.getElementById("chatbox");
+    //insertamos una nueva etiqueta html con el mensaje que recibimos
+    chatbox.innerHTML += `<p> ${id}: ${message} </p>`
+})
--- Clase10/src/app.js
+++ Clase10/src/app.js
@@ -23,11 +23,37 @@ app.get("/", (req,res)=>{
     res.render("index");
 });
 
+const messages = []; //Array de mensajes para guardar el historial del chat
+
 //websockets
 
+//Tiene que llamarse si o si "connection"
 io.on("connection", (socket)=>{
     //socket es un objeto que representa la conexion del cliente con el servidor
     console.log("Un nuevo usuario se conecto", socket.id)//id es el id del cliente que se loggea
+
+    //Podemos emitir eventos del lado del servidor hacia el cliente
+    socket.emit("welcome", messages);
+    
+    //Escucho los eventos que emite el cliente
+    socket.on("chatMessage", (newMessage)=>{
+        const message = {id: socket.id, message:newMessage}; //Creamos una variable con el id de quien esta enviando el mensaje y el mensaje enviado
+        messages.push(message);
+
+        //Para pasar el mansaje del servidor a TODOS los clientes
+        io.emit("broadcastMessage", message)//En lugar de socket.emit
+    }); //Socket.on se utiliza para escuchar eventos
+
+    //Escuchamos cuando se desconecta el cliente
+    //Tiene que llamarse si o si disconnect
+    socket.on("disconnect",(reason)=>{
+        console.log(`Cliente desconectado: ${reason}`);
+    });
+    //Razones para la desconexion:
+        //transport close: cierre de conexion
+        //ping timeout: falta de internet-la conexion del cleinte no responde al servidor
+        //server namespace disconnect: cuando el servidor desconecta al cliente
+            //esto ultimo s ehace con socket.disconnect(true)
 })
 
 //Como se manejaba anteriormente
@@ -41,7 +67,8 @@ app.use(express.static("public"));
 
 server.listen(PORT, ()=>{
     console.log("Servidor iniciado correctamente.");
-})
+});
+
 
 
 
